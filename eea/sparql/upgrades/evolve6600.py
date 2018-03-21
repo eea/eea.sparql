27f5b87336328086a10eac0d36d916e46de08617
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-03-21 17:19:27 +0100

    Refs #89670 Modified the upgrade step to use getSparqlCacheResults

--- eea/sparql/upgrades/evolve6600.py
+++ eea/sparql/upgrades/evolve6600.py
@@ -17,6 +17,7 @@ def set_sparql_export_attribute(context):
     log_count = 0
     migrated = 0
     negative_export = 0
+    no_result = 0
     for brain in brains:
         log_count += 1
         try:
@@ -25,14 +26,24 @@ def set_sparql_export_attribute(context):
             logger.info('FAILED getObject %s:%s: %s', log_count, log_total,
                         brain.getPath())
             continue
-        if getattr(obj, 'cached_result', None):
+
+        try:
+            cached_result = obj.getSparqlCacheResults()
+        except:
+            cached_result = None
+            logger.info('FAILED getgetSparqlCacheResults %s:%s: %s', log_count,
+            log_total, brain.getPath())
+            no_result += 1
+            continue
+
+        if cached_result:
             logger.info('PATH %s:%s: %s', log_count, log_total, brain.getPath())
 
             setattr(obj, 'exportWorks', True)
             try:
                 setattr(obj, 'exportWorks', True)
                 setattr(obj, 'exportStatusMessage', '')
-                sparql2json(obj.cached_result)
+                sparql2json(cached_result)
             except Exception, err:
                 logger.info('Export status failed on: %s', brain.getPath())
                 setattr(obj, 'exportWorks', False)
@@ -44,7 +55,10 @@ def set_sparql_export_attribute(context):
             if log_count % 100 == 0:
                 logger.info('INFO: Transaction committed to zodb (%s/%s)',
                             log_count, log_total)
+
     message = 'Finished setting export status for %s Sparqls' % migrated
     logger.info(message)
     logger.info('%s items have export status set to False' % negative_export)
+    logger.info('Number of items with no cached_result %s' % no_result)
+
     return message
