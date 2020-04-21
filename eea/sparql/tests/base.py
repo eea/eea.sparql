21580677b69b24602edc4e1741e41f385f861364
Petchesi-Iulian <iamnotacat12@gmail.com>
2020-04-21 16:55:48 +0200

    Refs #111217 Use layers for tests.

--- eea/sparql/tests/base.py
+++ eea/sparql/tests/base.py
@@ -10,9 +10,8 @@ from eea.sparql.tests.mock_server import PORT
 from eea.sparql.tests.mock_server import Handler
 
 from Products.Five import fiveconfigure
-from Products.Five import zcml
-from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import onsetup
+# from Products.PloneTestCase import PloneTestCase as ptc
+# from Products.PloneTestCase.layer import onsetup
 from Testing import ZopeTestCase as ztc
 #
 # Layered testing
@@ -26,43 +25,56 @@ from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import FunctionalTesting
 
 
-@onsetup
-def setup_sparql():
-    """Set up the additional products.
-    """
-    fiveconfigure.debug_mode = True
-    zcml.load_config('configure.zcml', eea.sparql)
-    fiveconfigure.debug_mode = False
-
-    ztc.installPackage('eea.sparql')
-
-
-setup_sparql()
-ptc.setupPloneSite(products=['eea.sparql'])
+try:
+    from Zope2.App import zcml
+    zcml._initialized = 0
+except ImportError:
+    pass
+try:
+    from Products.Five import zcml
+    zcml._initialized = 0
+except ImportError:
+    pass
 
 
-class SparqlFunctionalTestCase(ptc.FunctionalTestCase):
-    """ Base class for functional integration tests for the Sparql product.
-    """
-    def setUp(self):
-        """ Start the mock http server on port 8888
-            Since we make only one request we can use handle_request
-        """
-        self.server = six.moves.BaseHTTPServer.HTTPServer(("", PORT), Handler)
-        self.server_thread = threading.Thread(target=self.server.handle_request)
-        self.server_thread.start()
+# @onsetup
+# def setup_sparql():
+#     """Set up the additional products.
+#     """
+#     fiveconfigure.debug_mode = True
+#     zcml.load_config('configure.zcml', eea.sparql)
+#     fiveconfigure.debug_mode = False
 
-    def tearDown(self):
-        """ Stop the mock http server """
-        self.server.server_close()
-        self.server_thread.join()
+#     ztc.installPackage('eea.sparql')
 
-    def afterSetUp(self):
-        """ After setup """
-        roles = ('Member', 'Contributor')
-        self.portal.portal_membership.addMember('contributor',
-                                                'secret',
-                                                roles, [])
+
+# setup_sparql()
+# ptc.setupPloneSite(products=['eea.sparql'])
+
+
+# class SparqlFunctionalTestCase(ptc.FunctionalTestCase):
+#     """ Base class for functional integration tests for the Sparql product.
+#     """
+#     def setUp(self):
+#         """ Start the mock http server on port 8888
+#             Since we make only one request we can use handle_request
+#         """
+#         self.server = six.moves.BaseHTTPServer.HTTPServer(("", PORT), Handler)
+#         self.server_thread = threading.Thread(target=self.server.handle_request)
+#         self.server_thread.start()
+
+#     def tearDown(self):
+#         """ Stop the mock http server """
+#         self.server.server_close()
+#         self.server_thread.join()
+
+#     def afterSetUp(self):
+#         """ After setup """
+#         roles = ('Member', 'Contributor')
+#         self.portal.portal_membership.addMember('contributor',
+#                                                 'secret',
+#                                                 roles, [])
+
 
 class EEAFixture(PloneSandboxLayer):
     """ EEA Testing Policy
@@ -74,6 +86,7 @@ class EEAFixture(PloneSandboxLayer):
         """
         self.loadZCML(package=eea.sparql)
         z2.installProduct(app, 'eea.sparql')
+        z2.uninstallProduct(app, 'plone.app.imaging')
 
     def tearDownZope(self, app):
         """ Uninstall Zope
@@ -87,6 +100,7 @@ class EEAFixture(PloneSandboxLayer):
 
         # Login as manager
         setRoles(portal, TEST_USER_ID, ['Manager'])
+
 
 EEAFIXTURE = EEAFixture()
 FUNCTIONAL_TESTING = FunctionalTesting(bases=(EEAFIXTURE,),
--- eea/sparql/tests/test_doctest.py
+++ eea/sparql/tests/test_doctest.py
@@ -7,21 +7,35 @@ import doctest
 from Testing import ZopeTestCase as ztc
 
 from eea.sparql.tests import base
+from plone.testing import layered
+
 
 
 def test_suite():
     """ Suite
     """
-    return unittest.TestSuite([
+    # return unittest.TestSuite([
 
-        ztc.ZopeDocFileSuite(
-            'converter/sparql2json.py', package='eea.sparql',
-            test_class=base.SparqlFunctionalTestCase,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
-                doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
+    #     ztc.ZopeDocFileSuite(
+    #         'converter/sparql2json.py', package='eea.sparql',
+    #         test_class=base.SparqlFunctionalTestCaseLayer,
+    #         optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
+    #             doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
 
 
-        ])
+    #     ])
+
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(
+            doctest.DocFileSuite(
+                'converter/sparql2json.py',
+                optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+                package='eea.sparql'),
+            layer=base.FUNCTIONAL_TESTING),
+    ])
+    return suite
+
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
